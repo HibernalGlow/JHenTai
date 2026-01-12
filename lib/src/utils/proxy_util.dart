a7f5b605b7b50b824e38979fd61c2c5a07bb4db4
HibernalGlow <3090253564@qq.com>
2026-01-12 15:25:34 +0800

    release ok

--- lib/src/utils/proxy_util.dart
+++ lib/src/utils/proxy_util.dart
@@ -3,17 +3,12 @@ import 'package:get/get_utils/src/platform/platform.dart';
 import 'package:http_proxy/http_proxy.dart';
 import 'package:jhentai/src/service/log.dart';
 import 'package:jhentai/src/utils/string_uril.dart';
-import 'package:system_network_proxy/system_network_proxy.dart';
 
 import '../setting/network_setting.dart';
 
 Future<String> getSystemProxyAddress() async {
   String systemProxyAddress = '';
 
-  if (GetPlatform.isDesktop) {
-    SystemNetworkProxy.init();
-    systemProxyAddress = await SystemNetworkProxy.getProxyServer();
-  }
   if (GetPlatform.isMobile) {
     HttpProxy httpProxy = await HttpProxy.createHttpProxy();
     if (!isEmptyOrNull(httpProxy.host) && !isEmptyOrNull(httpProxy.port)) {
